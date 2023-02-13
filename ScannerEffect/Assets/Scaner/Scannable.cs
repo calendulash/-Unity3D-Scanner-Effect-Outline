36ed18203ba01dd5dd579b658e9c70b1017afa64
herjoon <anateanon@gmail.com>
2023-02-13 10:55:32 +0900

    Gitignore 파일 이동 및 첫 커밋

---
+++ ScannerEffect/Assets/Scaner/Scannable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scannable : MonoBehaviour
+{
+    public bool Pingflag = false;
+    public float ShowTimer = 0.0f;
+	public void Ping()
+	{
+        ShowTimer = 5.0f;
+        gameObject.SetActive(true);
+    }
+
+    public void Update()
+    {
+
+
+        ShowTimer -= Time.deltaTime;
+
+        if(ShowTimer <= 0.0f)
+        {
+            gameObject.SetActive(false);
+
+        }
+
+
+    }
+}
